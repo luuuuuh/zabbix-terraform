09fe12d1b66c7d91e8c94a0e6fb2b9ccd0fe1799
Luiz <look_lf@hotmail.com>
2019-12-16 13:19:15 -0300

    on-failure

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "docker"{
-   host = "tcp://localhost:2375/"
+   host = "tcp://lcoalhost:2375/"
 }
 resource "docker_network" "private_network" {
   name = "my_network"
@@ -10,6 +10,7 @@ resource "docker_container" "zabbix-server" {
   networks = [ "${docker_network.private_network.name}" ]
   env = ["MYSQL_ROOT_PASSWORD=zabbix", "MYSQL_PASSWORD=zabbix", "MYSQL_DATABASE=zabbix", "MYSQL_USER=zabbix"]
   name  = "zabbix-server"
+  restart = "on-failure"
   hostname = "zabbix-server"
      ports {
         internal = 10051
@@ -34,6 +35,7 @@ resource "docker_container" "zabbix-apache" {
   networks = [ "${docker_network.private_network.name}" ]
   env = ["ZBX_ENABLE_SNMP_TRAPS=true", "MYSQL_ROOT_PASSWORD=zabbix", "MYSQL_PASSWORD=zabbix", "MYSQL_DATABASE=zabbix"]
   name = "zabbix-apache"
+  restart = "on-failure"
   hostname = "zabbix-apache"
   ports {
         internal = 80
@@ -54,6 +56,7 @@ resource "docker_container" "mysql-server" {
   must_run = true
   networks = [ "${docker_network.private_network.name}" ]
   name = "mysql-server"
+  restart = "on-failure"
   hostname = "mysql-server"
   env = ["MYSQL_ROOT_PASSWORD=zabbix", "MYSQL_PASSWORD=zabbix", "MYSQL_DATABASE=zabbix"]
   command = ["mysqld", "--character-set-server=utf8", "--collation-server=utf8_bin", "--default-authentication-plugin=mysql_native_password"]
@@ -63,30 +66,30 @@ resource "docker_container" "mysql-server" {
     read_only = false
 }
 }
-resource "docker_container" "zabbix-agent" {
-  image = "${docker_image.zabbix-agent.latest}"
-  must_run = true
-  networks = [ "${docker_network.private_network.name}" ]
-  env = ["ZBX_HOSTNAME=Zabbix server"]
-  hostname = "zabbix-agent"
-  name = "zabbix-agent"
-    volumes {
-    container_path  = "/etc/localtime/"
-    host_path = "/etc/localtime/"
-    read_only = false
-}
-ports {
-        internal = 10050
-        external = 10050
-        }
-}
+#resource "docker_container" "zabbix-agent" {
+#  image = "${docker_image.zabbix-agent.latest}"
+#  must_run = true
+#  networks = [ "${docker_network.private_network.name}" ]
+ # env = ["ZBX_HOSTNAME=Zabbix server"]
+ # hostname = "zabbix-agent"
+ # name = "zabbix-agent"
+ #   volumes {
+ #   container_path  = "/etc/localtime/"
+ #   host_path = "/etc/localtime/"
+  #  read_only = false
+#}
+#ports {
+#        internal = 10050
+#        external = 10050
+#        }
+#}
 
 resource "docker_image" "zabbix-server" {
   name = "zabbix/zabbix-server-mysql:alpine-4.4-latest"
 }
-resource "docker_image" "zabbix-agent" {
-  name = "zabbix/zabbix-agent:alpine-4.4-latest"
-}
+#resource "docker_image" "zabbix-agent" {
+#  name = "zabbix/zabbix-agent:alpine-4.4-latest"
+#}
 resource "docker_image" "mysql" {
   name = "mysql:8.0"
 }
